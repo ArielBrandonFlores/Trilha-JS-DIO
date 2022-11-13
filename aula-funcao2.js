1ababa58063eaa5850e4a8fc2665c0c16c5aa10f
Dev-Flores <brandon.giron.flores@gmail.com>
2022-11-13 01:28:06 -0300

    Aula de funções

---
+++ aula-funcao2.js
@@ -0,0 +1,15 @@
+function escrevaMeuNome(nome){
+    console.log('Meu nome é ' + nome);
+}
+escrevaMeuNome('Ariel');
+escrevaMeuNome('Brandon');
+
+function verificarIdade(idade){
+    if (idade >= 18){
+        console.log('Maior de Idade');
+    } else {
+        console.log('Menor');
+    }
+}
+verificarIdade(23);
+verificarIdade(15);
